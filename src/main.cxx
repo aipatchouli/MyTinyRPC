6e86de0bc5279b0ae3e372e898a8c321a00830cc
aipatchouli <jiangsongmuq@gmail.com>
2022-11-12 15:26:50 +0800

    init RPC project

---
+++ src/main.cxx
@@ -0,0 +1,17 @@
+// This is a tiny project for RPC
+
+#include <cmath>
+#include <cstddef>
+#include <cstdio>
+#include <cstdlib>
+#include <iomanip>
+#include <iostream>
+#include <limits>
+#include <span>
+
+// argc is the number of arguments, argv is the array of arguments including "./main"
+auto main () -> int { 
+    
+
+    return 0;
+}
